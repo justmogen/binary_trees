9a7d92e3d86740e1885302905773375101d5df2a
justmogen <justmogen@gmail.com>
2023-03-03 23:49:45 +0300

    binary trees

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node -  creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * When created, a node does not have any child
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = value;
+	neNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return (newNode);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+0x1D. C - Binary trees
+Data structures
+Creating nodes,delete,add,search,tranverse nodes,values and roots

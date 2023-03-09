b1a767637ad734bbd8beb0335bd274123145015c
justmogen <justmogen@gmail.com>
2023-03-09 18:57:40 +0300

    corrrection

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,7 +15,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	int left_h = 0, right_h = 0;
+	int left_h = 0;
+	int right_h = 0;
 
 	if (tree->left != NULL)
 		left_h = binary_tree_height(tree->left) + 1;
